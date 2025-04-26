e8811e6c04f5d9b5ebfd5637ab1c10c0d4af5e9c
Hieu <hieu21042003a@gmail.com>
2025-04-26 09:59:19 +0700

    lần 2

--- obj/Debug/net6.0/WEB_SHOW_WRIST_STRAP.AssemblyInfo.cs
+++ obj/Debug/net6.0/WEB_SHOW_WRIST_STRAP.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEB_SHOW_WRIST_STRAP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9af378fec6c7c928153b8ec3053cf6178935853")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEB_SHOW_WRIST_STRAP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEB_SHOW_WRIST_STRAP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
